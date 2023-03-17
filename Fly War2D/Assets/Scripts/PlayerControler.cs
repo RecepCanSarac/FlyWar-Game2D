b9b06835e9637cd0f7443e2773008db83028316d
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-17 22:00:22 +0300

    Player Attack Okay

--- Fly War2D/Assets/Scripts/PlayerControler.cs
+++ Fly War2D/Assets/Scripts/PlayerControler.cs
@@ -6,7 +6,12 @@ public class PlayerControler : MonoBehaviour
 {
     public float speed;
     public float Vspeed;
+    public float Force;
     private Rigidbody2D rb;
+    public Transform shootPoint;
+    public GameObject bullet;
+    public float fireRate;
+    private float nextTimeFireRate = 0;
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -15,14 +20,21 @@ public class PlayerControler : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (Input.GetKey(KeyCode.Space) && Time.time > nextTimeFireRate)
+        {
+            nextTimeFireRate = Time.time + 1 / fireRate;
+            Shoot();
+        }
     }
     private void FixedUpdate()
     {
         float verticalMove = Input.GetAxisRaw("Vertical");
-        rb.velocity = new Vector2(speed * Time.deltaTime,verticalMove * Vspeed * Time.deltaTime);
-
-        
+        rb.velocity = new Vector2(speed * Time.deltaTime,verticalMove * Vspeed * Time.deltaTime);        
     }
-
+    private void Shoot()
+    {
+        GameObject bulletIns = Instantiate(bullet,shootPoint.position,Quaternion.identity);
+        bulletIns.GetComponent<Rigidbody2D>().AddForce(Vector2.right * Force);
+        Destroy(bulletIns,1.5f);
+    }
 }
