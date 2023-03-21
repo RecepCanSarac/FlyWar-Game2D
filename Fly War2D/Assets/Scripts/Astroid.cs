c06bc0f3b22954ff16699a5520bad58fa41cf86c
Recep Can Saraç <113214739+RecepCanSarac@users.noreply.github.com>
2023-03-21 19:16:57 +0300

    Update Astroid.cs

--- Fly War2D/Assets/Scripts/Astroid.cs
+++ Fly War2D/Assets/Scripts/Astroid.cs
@@ -23,4 +23,12 @@ public class Astroid : MonoBehaviour
     {
         rb.velocity = new Vector2(-Force * Time.deltaTime,random * Time.deltaTime);
     }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject)
+        {
+            Destroy(collision.gameObject);
+        }
+    }
 }
